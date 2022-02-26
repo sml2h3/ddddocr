334b4f97b08013b3a2fb948acf192358e9879f27
sml2h3 <sml2h3@gmail.com>
2022-02-26 15:41:57 +0800

    更新Readme文档

--- ddddocr/__init__.py
+++ ddddocr/__init__.py
@@ -5,6 +5,7 @@ warnings.filterwarnings('ignore')
 import io
 import os
 import base64
+import json
 import onnxruntime
 from PIL import Image, ImageChops
 import numpy as np
@@ -28,13 +29,26 @@ class TypeError(Exception):
 
 class DdddOcr(object):
     def __init__(self, ocr: bool = True, det: bool = False, old: bool = False, use_gpu: bool = False,
-                 device_id: int = 0, show_ad=True):
+                 device_id: int = 0, show_ad=True, import_onnx_path: str = "", charsets_path: str = ""):
         if show_ad:
             print("欢迎使用ddddocr，本项目专注带动行业内卷，个人博客:wenanzhe.com")
             print("训练数据支持来源于:http://146.56.204.113:19199/preview")
             print("爬虫框架feapder可快速一键接入，快速开启爬虫之旅：https://github.com/Boris-code/feapder")
-
-
+        self.use_import_onnx = False
+        self.__word = False
+        self.__resize = []
+        self.__channel = 1
+        if import_onnx_path != "":
+            det = False
+            ocr = False
+            self.__graph_path = import_onnx_path
+            with open(charsets_path, 'r', encoding="utf-8") as f:
+                info = json.loads(f.read())
+            self.__charset = info['charset']
+            self.__word = info['word']
+            self.__resize = info['image']
+            self.__channel = info['channel']
+            self.use_import_onnx = True
         if det:
             ocr = False
             print("开启det后自动关闭ocr")
@@ -1439,7 +1453,7 @@ class DdddOcr(object):
             self.__providers = [
                 'CPUExecutionProvider',
             ]
-        if ocr or det:
+        if ocr or det or self.use_import_onnx:
             self.__ort_session = onnxruntime.InferenceSession(self.__graph_path, providers=self.__providers)
 
     def preproc(self, img, input_size, swap=(2, 0, 1)):
@@ -1587,21 +1601,46 @@ class DdddOcr(object):
             image = Image.open(io.BytesIO(img_bytes))
         else:
             image = base64_to_image(img_base64)
-        image = image.resize((int(image.size[0] * (64 / image.size[1])), 64), Image.ANTIALIAS).convert('L')
+        if not self.use_import_onnx:
+            image = image.resize((int(image.size[0] * (64 / image.size[1])), 64), Image.ANTIALIAS).convert('L')
+        else:
+            if self.__resize[0] == -1:
+                if self.__word:
+                    image = image.resize((self.__resize[1], self.__resize[1]), Image.ANTIALIAS)
+                else:
+                    image = image.resize((int(image.size[0] * (self.__resize[1] / image.size[1])), self.__resize[1]), Image.ANTIALIAS)
+            else:
+                image = image.resize((self.__resize[0], self.__resize[1]), Image.ANTIALIAS)
+            if self.__channel == 1:
+                image = image.convert('L')
+            else:
+                image = image.convert('RGB')
         image = np.array(image).astype(np.float32)
         image = np.expand_dims(image, axis=0) / 255.
-        image = (image - 0.5) / 0.5
+        if not self.use_import_onnx:
+            image = (image - 0.5) / 0.5
+        else:
+            if self.__channel == 1:
+                image = (image - 0.456) / 0.224
+            else:
+                image = (image - np.array([0.485, 0.456, 0.406])) / np.array([0.229, 0.224, 0.225])
+
         ort_inputs = {'input1': np.array([image])}
         ort_outs = self.__ort_session.run(None, ort_inputs)
         result = []
+
         last_item = 0
-        for item in ort_outs[0][0]:
-            if item == last_item:
-                continue
-            else:
-                last_item = item
-            if item != 0:
+        if self.__word:
+            for item in ort_outs[1]:
                 result.append(self.__charset[item])
+        else:
+            for item in ort_outs[0][0]:
+                if item == last_item:
+                    continue
+                else:
+                    last_item = item
+                if item != 0:
+                    result.append(self.__charset[item])
 
         return ''.join(result)
 
--- test.py
+++ test.py
@@ -1,52 +1,9 @@
 import ddddocr
 
-# # 识别
-# ocr = ddddocr.DdddOcr(old=True)
-#
-# with open('test.jpg', 'rb') as f:
-#     img_bytes = f.read()
-#
-# res = ocr.classification(img_bytes)
-# print(res)
+ocr = ddddocr.DdddOcr(det=False, ocr=False, import_onnx_path="myproject_0.984375_139_13000_2022-02-26-15-34-13.onnx", charsets_path="charsets.json")
 
-# 目标检测
-# det = ddddocr.DdddOcr(det=True)
-#
-# with open('微信图片_20220123122537.jpg', 'rb') as f:
-#     img_bytes = f.read()
-#
-# res = det.detection(img_bytes)
-# print(res)
+with open('888e28774f815b01e871d474e5c84ff2.jpg', 'rb') as f:
+    image_bytes = f.read()
 
-# 滑块模板匹配方式
-import cv2
-
-im = cv2.imread("b.png")
-det = ddddocr.DdddOcr(det=False, ocr=False)
-
-with open('t.png', 'rb') as f:
-    target_bytes = f.read()
-
-with open('b.png', 'rb') as f:
-    background_bytes = f.read()
-
-res = det.slide_match(target_bytes, background_bytes)
+res = ocr.classification(image_bytes)
 print(res)
-x = res["target"]
-print(x)
-# im = cv2.line(im, (int(x[0]), 0), (int(x[0]), 200), color=(0, 0, 255), thickness=2)
-im = cv2.rectangle(im, (int(x[0]), int(x[1])), (int(x[2]), int(x[3])), color=(0, 0, 255), thickness=2)
-cv2.imwrite("res.jpg", im)
-
-# det = ddddocr.DdddOcr(det=False, ocr=False)
-#
-# with open('bg.jpg', 'rb') as f:
-#     target_bytes = f.read()
-#
-# with open('fullpage.jpg', 'rb') as f:
-#     background_bytes = f.read()
-#
-#
-# res = det.slide_comparison(target_bytes, background_bytes)
-#
-# print(res)
